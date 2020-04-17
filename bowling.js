b4c0860fda9d6da86b9fa1a39f72865143a4d52d
dmills <d.mills@vehikl.com>
2020-04-17 15:00:53 -0400

    make first test past

--- bowling.js
+++ bowling.js
@@ -5,10 +5,9 @@
 
 export class Bowling {
   roll() {
-    throw new Error("Remove this statement and implement this function");
   }
 
   score() {
-    throw new Error("Remove this statement and implement this function");
+    return 0;
   }
 }
