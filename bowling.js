bf638b2b0b91603635d8fae060d7906c2510d735
Nick Down <nick@nickdown.com>
2020-04-17 15:15:11 -0400

    Count spares properly

--- bowling.js
+++ bowling.js
@@ -17,7 +17,15 @@ export class Bowling {
     var final = 0;
     var rollCounter = 0;
     for(var i =0; i< 10; i++) {
+      const frame = this.rolls.splice(0, 2);
+      let frameScore = frame.reduce((accumulator, value) => accumulator + value, 0);
 
+      if (frameScore === 10) {
+        frameScore += this.rolls[0];
+      }
+
+
+      final += frameScore;
     }
 
     return final;
